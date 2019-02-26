71f6abacbb91bb66e4710b8a4d7e3e627f5f12b3
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2019-02-25 16:18:04 -0800

    committing initial work for basic visualizing of audio

---
+++ Vizualize R/Assets/_Scripts/ParamCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(transform.localScale.x, (Vizualize_R._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+	}
+}
---
+++ Vizualize R/Assets/_Scripts/Vizualize_R.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class Vizualize_R : MonoBehaviour {
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+
+	// Use this for initialization
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for(int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= sampleCount;
+
+            _freqBand[i] = average * 10;
+        }
+    }
+}
